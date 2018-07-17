9190d11ffd4ae46617cbdb78e018e4891a54763c
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-07-17 10:57:07 +0400

    feat: Plant Timber

--- app/src/main/java/app/ogasimli/remoter/app/RemoterApp.kt
+++ app/src/main/java/app/ogasimli/remoter/app/RemoterApp.kt
@@ -8,8 +8,11 @@
 package app.ogasimli.remoter.app
 
 import androidx.multidex.MultiDexApplication
-import app.ogasimli.remoter.di.component.AppComponent
+import app.ogasimli.remoter.BuildConfig
 import app.ogasimli.remoter.di.component.DaggerAppComponent
+import app.ogasimli.remoter.helper.timber.DebugLogTree
+import app.ogasimli.remoter.helper.timber.ReleaseLogTree
+import timber.log.Timber
 
 /**
  * Application class
@@ -23,6 +26,9 @@ class RemoterApp : MultiDexApplication() {
 
         // Inject Dagger
         injectDagger()
+
+        // Plant Timber
+        plantTimber()
     }
 
     /**
@@ -33,5 +39,20 @@ class RemoterApp : MultiDexApplication() {
                 .builder()
                 .application(this)
                 .build()
+                .inject(this)
+    }
+
+    /**
+     * Helper method to plan Timber logging trees.
+     *
+     * If app is in debug mode, then plant {@link DebugLogTree},
+     * else plant {@link ReleaseLogTree}
+     */
+    private fun plantTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugLogTree())
+        } else {
+            Timber.plant(ReleaseLogTree())
+        }
     }
 }
