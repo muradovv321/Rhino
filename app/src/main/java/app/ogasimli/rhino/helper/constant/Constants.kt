0985e82cc0d10cbac551ee8b41c6549e3aee73df
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-08-25 16:02:18 +0400

    refactor: Use const instead of @JvmStatic annotation in Constants class

--- app/src/main/java/app/ogasimli/rhino/helper/constant/Constants.kt
+++ app/src/main/java/app/ogasimli/rhino/helper/constant/Constants.kt
@@ -5,6 +5,8 @@
  * Written by Orkhan Gasimli orkhan.gasimli@gmail.com in 2018.
  */
 
+@file:JvmName("Constants")
+
 package app.ogasimli.rhino.helper.constant
 
 import app.ogasimli.rhino.BuildConfig
@@ -19,68 +21,57 @@ object Constants {
     /**
      * Package firstName of the app
      */
-    @JvmStatic
-    private val PACKAGE_NAME = BuildConfig.APPLICATION_ID
+    private const val PACKAGE_NAME = BuildConfig.APPLICATION_ID
 
     /**
      * Default key of SharedPreferences
      */
-    @JvmStatic
-    val DEFAULT_SHARED_PREF_KEY = "$PACKAGE_NAME.SHARED_PREF_KEY"
+    const val DEFAULT_SHARED_PREF_KEY = "$PACKAGE_NAME.SHARED_PREF_KEY"
 
     /**
      * SharedPreferences key for {@link SortOption} of all jobs
      */
-    @JvmStatic
-    val SORT_OPTION_ALL_KEY = "$PACKAGE_NAME.SORT_OPTION_ALL_KEY"
+    const val SORT_OPTION_ALL_KEY = "$PACKAGE_NAME.SORT_OPTION_ALL_KEY"
 
     /**
      * SharedPreferences key for {@link SortOption} of bookmarked jobs
      */
-    @JvmStatic
-    val SORT_OPTION_BOOKMARKED_KEY = "$PACKAGE_NAME.SORT_OPTION_BOOKMARKED_KEY"
+    const val SORT_OPTION_BOOKMARKED_KEY = "$PACKAGE_NAME.SORT_OPTION_BOOKMARKED_KEY"
 
     /**
      * SharedPreferences key for {@link FilterOption} of all jobs
      */
-    @JvmStatic
-    val FILTER_OPTION_ALL_KEY = "$PACKAGE_NAME.FILTER_OPTION_ALL_KEY"
+    const val FILTER_OPTION_ALL_KEY = "$PACKAGE_NAME.FILTER_OPTION_ALL_KEY"
 
     /**
      * SharedPreferences key for {@link FilterOption} of bookmarked jobs
      */
-    @JvmStatic
-    val FILTER_OPTION_BOOKMARKED_KEY = "$PACKAGE_NAME.FILTER_OPTION_BOOKMARKED_KEY"
+    const val FILTER_OPTION_BOOKMARKED_KEY = "$PACKAGE_NAME.FILTER_OPTION_BOOKMARKED_KEY"
 
     /**
      * Key of the job item to pass between activities via the Bundle
      */
-    @JvmStatic
-    val JOB_ITEM_BUNDLE_KEY = "$PACKAGE_NAME.JOB_ITEM_BUNDLE_KEY"
+    const val JOB_ITEM_BUNDLE_KEY = "$PACKAGE_NAME.JOB_ITEM_BUNDLE_KEY"
 
     /**
      * Timeout limit of http requests in seconds
      */
-    @JvmStatic
-    val OK_HTTP_TIMEOUT = 15L // 15 seconds
+    const val OK_HTTP_TIMEOUT = 15L // 15 seconds
 
     /**
      * Folder firstName of OkHttp cache
      */
-    @JvmStatic
-    val OK_HTTP_CACHE_FOLDER_NAME = "http-cache"
+    const val OK_HTTP_CACHE_FOLDER_NAME = "http-cache"
 
     /**
      * Max cache size of OkHttp
      */
-    @JvmStatic
-    val OK_HTTP_MAX_CACHE_SIZE = 10L * 1024 * 1024 // 10 MB
+    const val OK_HTTP_MAX_CACHE_SIZE = 10L * 1024 * 1024 // 10 MB
 
     /**
      * Max age of http requests in minutes
      */
-    @JvmStatic
-    val OK_HTTP_MAX_AGE = 10
+    const val OK_HTTP_MAX_AGE = 10
 
     /**
      * Cache expiration time for
@@ -92,12 +83,10 @@ object Constants {
     /**
      * Remote config key for fetching 'from' query parameter
      */
-    @JvmStatic
-    val DATA_SOURCE_KEY = "data_source"
+    const val DATA_SOURCE_KEY = "data_source"
 
     /**
      * Base URL for fetching coin data
      */
-    @JvmStatic
-    val API_BASE_URL = "https://rhino-app.herokuapp.com/v1/"
+    const val API_BASE_URL = "https://rhino-app.herokuapp.com/v1/"
 }
